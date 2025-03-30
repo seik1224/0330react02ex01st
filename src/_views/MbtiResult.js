095c4334e859bf3905b8bb19b0674870c8d290d2
seikSBS <seik1224@naver.com>
2025-03-30 12:11:29 +0900

    .

--- src/_views/MbtiResult.js
+++ src/_views/MbtiResult.js
@@ -9,7 +9,7 @@ function MbtiResult({name, mbti}) {
       try {
         const response = await fetch('https://raw.githubusercontent.com/seik1224/data-zelda/main/mbti.json');
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
 
         const result = data.find((item)=>item.type === mbti)
         console.log(result);
@@ -25,19 +25,23 @@ function MbtiResult({name, mbti}) {
   return (
     <div className="text-center">
       <div className="flex flex-col justify-center items-center relative z-10">
-        <>
-          <img
-            className="w-[200px] bg-blend-darken"
-            src="/images/entj.jpg"
-            alt="mbti이미지"
-          />
-          <h2
-            className="text-2xl font-bold mb-4 px-6 py-2 -mt-10 rounded-full border-[6px] bg-white box-border"
-            style={{ borderColor: "black", color: "black" }}
-          >
-            이름
-          </h2>
-        </>
+        {
+          mbtiData &&
+          <>
+            <img
+              className="w-[200px] bg-blend-darken"
+              src={process.env.PUBLIC_URL + mbtiData.image}
+              alt={mbtiData.type}
+            />
+            <h2
+              className="text-2xl font-bold mb-4 px-6 py-2 -mt-10 rounded-full border-[6px] bg-white box-border"
+              style={{ borderColor: mbtiData.color, color: mbtiData.color }}
+            >
+              {name}
+            </h2>
+          </>
+        }
+        
       </div>
 
       <MbtiProfile />
